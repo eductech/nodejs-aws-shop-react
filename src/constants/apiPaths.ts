798f2c56a8bbd17baff3c08f45b185dae962412a
Aliaksandr Zamirouski <alex.zamirouski@gmail.com>
2024-01-09 23:23:10 +0300

    endpoints fix

--- src/constants/apiPaths.ts
+++ src/constants/apiPaths.ts
@@ -1,9 +1,9 @@
 const API_PATHS = {
-  product: "http://eductech-bff-api-dev.eu-north-1.elasticbeanstalk.com",
+  product: "http://eductech-bff-api-dev.eu-north-1.elasticbeanstalk.com/products",
   order: "http://eductech-aws-cart-api-dev.eu-north-1.elasticbeanstalk.com",
   import: "https://1cixh8npm5.execute-api.eu-north-1.amazonaws.com",
   bff: "https://.execute-api.eu-west-1.amazonaws.com/dev",
-  cart: "http://eductech-bff-api-dev.eu-north-1.elasticbeanstalk.com",
+  cart: "http://eductech-bff-api-dev.eu-north-1.elasticbeanstalk.com/cart/api",
 };
 
 export default API_PATHS;
